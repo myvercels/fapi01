0ab6712e60c93ebde2a949fac50eda4003589246
myvercels <myvercel>
2025-08-05 22:30:14 +0800

    555

--- 123.py
+++ 123.py
@@ -1,5 +1,7 @@
 # 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
 
+# git remote set-url origin https://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@github.com/myvercels/fapi01/ 
+
 from fastapi import FastAPI, Response
 app = FastAPI()
 
